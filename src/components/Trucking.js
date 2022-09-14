b2f24722dd242b6530f1e5a47bc9a4018ab3aa41
ivanedrei <ivanramirez2014@gmail.com>
2022-09-14 15:23:04 -0500

    login and register working but Home page does not render; need to fix the 5 error messages that pop up too much

--- src/components/Trucking.js
+++ src/components/Trucking.js
@@ -1,7 +1,7 @@
 import "./Trucking.css"
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
-import { ApplicationViews } from "./ApplicationViews"
+import { ApplicationViews } from "../ApplicationViews"
 import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
