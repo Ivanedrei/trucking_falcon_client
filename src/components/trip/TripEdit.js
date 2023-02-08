08092f59eebde75d115250110b184bd83d6272a9
ivanedrei <ivanramirez2014@gmail.com>
2023-02-08 08:55:34 -0600

    edit form css

--- src/components/trip/TripEdit.js
+++ src/components/trip/TripEdit.js
@@ -77,168 +77,54 @@ export const TripEdit = () => {
         <form className="tripForm">
             <h2 className="tripForm__title title">Update Trip</h2>
             <div className="flex_form">
-                <div className="form-group input_form">
-                    <label htmlFor="from_address" className="description">Start Address:</label>
+                <div className="form-group input_form1">
+                    <label htmlFor="from_address" ></label>
                     <input type="text" id="from_address" onChange={handleFieldChange} required autoFocus
-                        className="form-control" placeholder={trip.from_address} value={trip.from_address} />
+                        className="form-control1" placeholder={trip.from_address} value={trip.from_address} />
                 </div>
-                <div className="form-group input_form">
-                    <label htmlFor="destination" className="description">Destination:</label>
+                <div className="form-group input_form1">
+                    <label htmlFor="destination" ></label>
                     <input type="text" id="destination" onChange={handleFieldChange} required autoFocus
-                        className="form-control" placeholder={trip.destination} value={trip.destination} />
+                        className="form-control1" placeholder={trip.destination} value={trip.destination} />
                 </div>
-                <div className="form-group input_form">
-                    <label htmlFor="startDate" className="description">Start Date:</label>
+                <div className="form-group input_form1">
+                    <label htmlFor="startDate" ></label>
                     <input type="text" id="start_date" onChange={handleFieldChange} required autoFocus
-                        className="form-control" placeholder={trip.start_date} value={trip.start_date} />
+                        className="form-control1" placeholder={trip.start_date} value={trip.start_date} />
                 </div>
-                <div className="form-group input_form">
-                    <label htmlFor="totalMiles" className="description">Total Miles:</label>
+                <div className="form-group input_form1">
+                    <label htmlFor="finishDate"></label>
+                    <input type="text" id="finish_date" onChange={handleFieldChange} required autoFocus
+                        className="form-control1" placeholder={trip.finish_date} value={trip.finish_date} />
+                </div>
+                <div className="form-group input_form1">
+                    <label htmlFor="totalMiles"></label>
                     <input type="number" id="total_miles" onChange={handleFieldChange} required autoFocus
-                        className="form-control" placeholder={trip.total_miles} value={trip.total_miles} />
+                        className="form-control1" placeholder={trip.total_miles} value={trip.total_miles} />
                 </div>
-                <section className="flex_row input_form">
-                    <div className="form-group">
-                        <label htmlFor="truck" className="description">Plate Number: </label>
-                        <select name="truck" className="form-control" id="truck"
-                            value={trip.truck} //your getting the id from the useEffect on line 64
-                            onChange={handleFieldChange}>
-                            <option value=""> select one </option>
-                            {trucks.map(truck => (
-                                <option key={truck.id} value={truck.id}> {truck.plate_number}</option>
-                            ))} </select>
-                    </div>
-                    <div>
-                        <label className="description input_form"> Loaded Truck?</label>
-                        <input type="checkbox" checked={checked} onChange={handleChange} id="checked"></input>
+                {/* <section className="flex_row input_form1"> */}
+                <div className="form-group input_form2">
+                    <label htmlFor="truck" className="dec_edit">Plate Number: </label>
+                    <select name="truck" className="form-control" id="truck"
+                        value={trip.truck} //your getting the id from the useEffect on line 64
+                        onChange={handleFieldChange}>
+                        <option value=""> select one </option>
+                        {trucks.map(truck => (
+                            <option key={truck.id} value={truck.id}> {truck.plate_number}</option>
+                        ))} </select>
+                </div>
+                <div className="form-group input_form2">
+                    <label className="dec_edit "> Loaded Truck?</label>
+                    <input type="checkbox" checked={checked} onChange={handleChange} id="checked"></input>
 
-                    </div>
-                </section>
-                <div>
-                    <div className="form-group input_form">
-                        <label htmlFor="finishDate" className="description">Finish Date:</label>
-                        <input type="text" id="finish_date" onChange={handleFieldChange} required autoFocus
-                            className="form-control" placeholder={trip.finish_date} value={trip.finish_date} />
-                    </div>
                 </div>
+                {/* </section> */}
             </div>
             <div className="flexy">
                 <button type="submit" disabled={isLoading}
                     onClick={updateExistingTrip}
-                    // onClick={evt => {
-                    //     // Prevent form from being submitted
-                    //     evt.preventDefault()
-
-                    //     const updateTrip = {
-                    //         from_address: trip.from_address,
-                    //         destination: trip.destination,
-                    //         start_date: parseInt(trip.start_date),
-                    //         total_miles: parseInt(trip.total_miles),
-                    //         truck: parseInt(trip.truck),
-                    //         loaded: parseInt(trip.loaded),
-                    //         finish_date: parseInt(trip.finish_date)
-                    //     }
-
-                    //         // Send POST request to your API
-
-
-                    //         (updateMyTrip(updateTrip, id)
-                    //             .then(() => history.push("/trips")))
-                    //     console.log(updateTrip)
-                    // }
-                    // } 
                     className="btn_1 btn" > Save </button>
             </div>
         </form >
     )
 }
-
-
-// import React, { useState, useEffect } from "react"
-// import { useHistory, useParams } from 'react-router-dom'
-// import { getTrucks } from "../truck/truckManager"
-// import { getTripById, updateMyTrip } from "./tripManager"
-
-
-// export const TripEdit = () => {
-//     const history = useHistory()
-//     const [trucks, setTrucks] = useState([])
-//     const [isLoading, setIsLoading] = useState(false);
-
-
-//     const { tripId } = useParams()
-
-//     const [trip, setTrip] = useState({
-//         employee: 1,
-//         from_address: "",
-//         destination: "",
-//         start_date: "2022-09-22",
-//         total_miles: 1,
-//         truck: 1,
-//         // loaded: checked,
-//         finish_date: "2022-09-23"
-//     })
-
-//     const updateExistingTrip = evt => {
-//         // Prevent form from being submitted
-//         evt.preventDefault()
-//         setIsLoading(true);
-
-
-//         const updateTrip = {
-//             id: tripId,
-//             from_address: trip.from_address,
-//             destination: trip.destination,
-//             start_date: parseInt(trip.start_date),
-//             total_miles: parseInt(trip.total_miles),
-//             truck: parseInt(trip.truck),
-//             loaded: parseInt(trip.loaded),
-//             finish_date: parseInt(trip.finish_date)
-//         }
-//         updateMyTrip(updateTrip)
-//             .then(() => history.push("/trips"))
-//         console.log(updateTrip)
-//     }
-
-//     useEffect(() => {
-//         getTrucks().then(setTrucks)
-//         if (tripId) {
-//             getTripById(tripId)
-//                 .then(editTrip => {
-//                     setTrip({
-//                         id: editTrip.id,
-//                         from_address: editTrip.from_address,
-//                         destination: editTrip.destination,
-//                         start_date: editTrip.start_date,
-//                         total_miles: editTrip.total_miles,
-//                         truck: editTrip.truck,
-//                         loaded: editTrip.loaded,
-//                         finish_date: editTrip.finish_date
-//                     });
-//                     setIsLoading(false);
-//                 })
-//         }
-//     }, [])
-
-//     const changeTripState = (domEvent) => {
-//         const newTrip = { ...trip }
-//         let selectedVal = domEvent.target.value
-//         if (domEvent.target.id.includes("Id")) {
-//             selectedVal = parseInt(selectedVal)
-//         }
-//         newTrip[domEvent.target.name] = selectedVal
-//         setTrip(newTrip)
-//     }
-
-//     // test from animalEditForm: Kennels
-//     // const handleFieldChange = evt => {
-//     //     const stateToChange = { ...animal };
-//     //     stateToChange[evt.target.id] = evt.target.value;
-//     //     setAnimal(stateToChange);
-//     // };
-
-    // const handleChange = () => {
-    //     setChecked(!checked);
-    // };
-
-    // const [checked, setChecked] = useState({})
